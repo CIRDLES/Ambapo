7c29ccc53a6821adf459d86e7445fe1759f3344b
ecoli1996 <coleev@g.cofc.edu>
2017-09-05 00:53:39 -0400

    started JavaFX version of gui

---
+++ app/src/main/java/org/cirdles/ambapo/userInterface/AmbapoUIController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cirdles.ambapo.userInterface;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author evc1996
+ */
+public class AmbapoUIController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
